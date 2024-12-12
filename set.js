89a0b16740e9eb8a8dcc652e5ad2d0f410f32215
FredieTech <frediezra60@gmail.com>
2024-12-12 09:48:17 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,9 +1,9 @@
 module.exports = {
   PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
+  NOM_OWNER: process.env.NOM_OWNER || "Fredi",
   NUMERO_OWNER: process.env.NUMERO_OWNER || "",
   MODE: process.env.MODE || "public",
-  MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  MENU: process.env.MENU || "https://files.catbox.moe/7irwqn.jpeg",
+  SESSION_ID: process.env.SESSION_ID || "lucky",
   DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 };
